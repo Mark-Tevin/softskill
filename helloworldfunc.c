f4e9e375aa5b1f644d0dc0caba408d2c09370857
Mark-Tevin <tevinsmark@gmail.com>
2023-07-13 22:22:17 -0700

    trialpush

---
+++ helloworldfunc.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/*function to print hello world without using printf*/
+void printMessage(void)
+{
+	printf("Hello world\n");
+}
+
+int main(void)
+{
+	/*calling the function to print*/
+	printMessage();
+
+	return (0);
+}
